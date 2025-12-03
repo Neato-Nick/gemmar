355e35df254dbee27ca152fc3768135c5e0bc806
Neato-Nick <nc.cauldron@gmail.com>
2025-12-03 16:15:08 -0500

    Improve lambda labeling

--- R/vcf_parsing_helpers.R
+++ R/vcf_parsing_helpers.R
@@ -769,7 +769,8 @@ gemma2QQ <- function(df, use_p_val = "p_lrt", show_lambda = TRUE) {
         geom = "text",
         x = min(df$negLog10_expected), # Position at the minimum x-value
         y = max(pull(df, !!sym(paste0("negLog10_", use_p_val))))*0.9, # Position at the maximum y-value
-        label = round(lambda, digits = 4), hjust = 0, vjust = 1
+        label = expression(paste0(lambda["gc"], " = ", round(lambda, digits = 4))),
+        hjust = 0, vjust = 1
       )
   }
   return(qq_gg)
